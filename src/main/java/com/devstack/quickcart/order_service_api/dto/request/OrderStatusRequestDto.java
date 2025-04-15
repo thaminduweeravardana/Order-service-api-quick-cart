b722d4dc00dbac2110d33ebaba39baefa6dc7ee5
thaminduweeravardana <thamindu2003github@gmail.com>
2025-04-15 23:19:19 +0530

    Done create status method ✅

--- src/main/java/com/devstack/quickcart/order_service_api/dto/request/OrderStatusRequestDto.java
+++ src/main/java/com/devstack/quickcart/order_service_api/dto/request/OrderStatusRequestDto.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderStatusRequestDto {
+    private String orderId;
     private String status;
     private ArrayList<CustomerOrderRequestDto> customerOrder;
 }
--- src/main/java/com/devstack/quickcart/order_service_api/service/impl/OrderStatusServiceImpl.java
+++ src/main/java/com/devstack/quickcart/order_service_api/service/impl/OrderStatusServiceImpl.java
@@ -3,12 +3,37 @@ package com.devstack.quickcart.order_service_api.service.impl;
 import com.devstack.quickcart.order_service_api.dto.request.OrderStatusRequestDto;
 import com.devstack.quickcart.order_service_api.dto.response.OrderStatusResponseDto;
 import com.devstack.quickcart.order_service_api.dto.response.paginate.OrderStatusPaginateDto;
+import com.devstack.quickcart.order_service_api.entity.CustomerOrder;
+import com.devstack.quickcart.order_service_api.entity.OrderStatus;
+import com.devstack.quickcart.order_service_api.exception.EntryNotFoundException;
+import com.devstack.quickcart.order_service_api.repo.CustomerOrderRepo;
+import com.devstack.quickcart.order_service_api.repo.OrderStatusRepo;
 import com.devstack.quickcart.order_service_api.service.OrderStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class OrderStatusServiceImpl implements OrderStatusService {
+
+    private final OrderStatusRepo orderStatusRepo;
+    private final CustomerOrderRepo customerOrderRepo;
+
     @Override
     public void createStatus(OrderStatusRequestDto requestDto) {
+        Set<CustomerOrder> customerOrderSet = customerOrderRepo.findByOrderId(requestDto.getOrderId())
+                .stream().collect(Collectors.toSet());
 
+        OrderStatus orderStatus = new OrderStatus();
+        orderStatus.setStatusId(UUID.randomUUID().toString());
+        orderStatus.setStatus(requestDto.getStatus());
+        orderStatus.setCustomerOrders(customerOrderSet);
+        orderStatusRepo.save(orderStatus);
     }
 
     @Override
